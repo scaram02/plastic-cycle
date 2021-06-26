2e40089fcd195635a87471c7c4366e9323c75e75
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-06-25 21:42:30 -0500

    upload basic code

---
+++ src/components/AddPlastic.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const AddPlastic = () => {
+
+
+
+    return (
+        <div>
+            add 
+        </div>
+    )
+}
+
+
+export default AddPlastic
---
+++ src/components/EditPlastic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EditPlastic = () => {
+    return (
+        <div>
+            edit
+        </div>
+    )
+}
+
+
+export default EditPlastic
